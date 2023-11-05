52526cc5be1baf3af5e8a1a1d4cd8099e8141155
Akraminum <akraminum@gmail.com>
2023-11-05 09:45:54 +0200

    .

--- Tasks/views.py
+++ Tasks/views.py
@@ -6,9 +6,19 @@ from .Serializers import TaskModelOutputSerializer, TaskModeInputSerializer
 
 class TaskModelDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TaskModel.objects.all()
-    serializer_class = TaskModelOutputSerializer
+    # serializer_class = TaskModelOutputSerializer
     lookup_field = 'id'
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return TaskModelOutputSerializer
+        return TaskModeInputSerializer
 
 class TaskModelListAPIView(generics.CreateAPIView):
     queryset = TaskModel.objects.all()
-    serializer_class = TaskModeInputSerializer
+    # serializer_class = TaskModeInputSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return TaskModelOutputSerializer
+        return TaskModeInputSerializer
